95ac88725e5a2d903fc947ab46e2edb33cad26b3
Muhammad Aftab <you@example.com>
2026-06-23 19:36:19 +0530

    Added Quran API

--- src/controllers/quranController.js
+++ src/controllers/quranController.js
@@ -65,3 +65,106 @@ error:error.message
 }
 
 };
+exports.getTodayQuran =
+async (req,res)=>{
+
+try{
+
+const result =
+await pool.query(
+
+`
+SELECT *
+FROM quran_records
+ORDER BY reading_date DESC
+LIMIT 1
+`
+
+);
+
+res.json(
+result.rows[0]
+);
+
+}
+
+catch(error){
+
+console.error(error);
+
+res.status(500).json({
+error:error.message
+});
+
+}
+
+};exports.getQuranAnalytics =
+async (req,res)=>{
+
+  try{
+
+    res.json({
+
+      currentStreak: 0,
+
+      highestStreak: 0,
+
+      monthMinutes: 0,
+
+      yearMinutes: 0,
+
+      topSurahMonth: "No Data",
+
+      topSurahYear: "No Data"
+
+    });
+
+  }
+
+  catch(error){
+
+    res.status(500).json({
+      error:error.message
+    });
+
+  }
+
+};
+
+exports.getQuranTrend =
+async (req,res)=>{
+
+  try{
+
+    res.json([]);
+
+  }
+
+  catch(error){
+
+    res.status(500).json({
+      error:error.message
+    });
+
+  }
+
+};
+
+exports.getSurahProgress =
+async (req,res)=>{
+
+  try{
+
+    res.json([]);
+
+  }
+
+  catch(error){
+
+    res.status(500).json({
+      error:error.message
+    });
+
+  }
+
+};
